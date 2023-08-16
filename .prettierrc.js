442b16ab3281aee441d3d09e627b6123fff4b0aa
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-08-15 23:17:55 -0300

    Installed packages and set up Prettier
    Installed Makerd and Puppeteer dependencies
    Installed Prettier, Nodemon, TS-Node and Typescript dev dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	arrowParens: "always",
+	bracketSpacing: true, // Simple spacing between brackets
+	endOfLine: "crlf",
+	htmlWhitespaceSensitivity: "css", // HTML whitespace sensitivity
+	insertPragma: false, // Insert @format at the top of the file
+	jsxSingleQuote: false, // Use single quotes in JSX
+	proseWrap: "always",
+	quoteProps: "as-needed",
+	requirePragma: false, // Require a @format pragma to format files
+	semi: true, // Use semicolons at the end of statements
+	singleQuote: true, // Use single quotes instead of double quotes
+	printWidth: 120, // Maximum line width
+	tabWidth: 4, // Number of spaces per indentation level
+	trailingComma: "all", // Trailing commas wherever possible
+	useTabs: false, // Use tabs instead of spaces
+	vueIndentScriptAndStyle: false,
+	embeddedLanguageFormatting: "auto",
+};
